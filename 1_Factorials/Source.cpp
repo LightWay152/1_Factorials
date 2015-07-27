1c32c3b39711363c3e712e405cd0b2b4e68145f4
Light Way <lightwayisme@gmail.com>
2015-07-27 17:49:49 +0700

    https://github.com/LightWay152/1_Factorials.git

---
+++ 1_Factorials/Source.cpp
@@ -0,0 +1,33 @@
+﻿/*Tính giai thừa của một số nguyên*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int Factorials(int n) // by Recursion
+{
+	if (n <= 2)
+		return n;
+	else // n>2
+		return n*Factorials(n - 1);
+}
+
+void main()
+{
+	int n;
+	char choose;
+
+	printf("\n --- Factorials---");
+	do
+	{
+		printf("\n\n Enter the integer n: ");
+		scanf("%d", &n);
+
+		int S = Factorials(n);
+
+		printf("\n Factorial of %d is: %d", n, S);
+		printf("\n\n Press y to continue or any keyboard to exit: ");
+
+		choose = getch();
+	} while (choose == 'y');
+
+}
